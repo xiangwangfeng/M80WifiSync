14bd3a079966fb44b27f777ed20fc58ff1fb2e45
xiangwangfeng <xiangwangfeng@gmail.com>
2015-03-03 13:24:48 +0800

    添加对网络变化的监听

--- Classes/Sections/HttpServer/M80ServerViewController.m
+++ Classes/Sections/HttpServer/M80ServerViewController.m
@@ -11,28 +11,31 @@
 #import "M80DirectoryViewController.h"
 #import "M80PathManager.h"
 #import "M80Kit.h"
+#import "Reachability.h"
 
 @interface M80ServerViewController ()
 @property (strong, nonatomic) IBOutlet UIButton *filesButton;
 @property (strong, nonatomic) IBOutlet UILabel *linkLabel;
 @property (strong, nonatomic) IBOutlet UILabel *networkLabel;
 @property (strong, nonatomic) M80HttpServer *server;
+@property (strong, nonatomic) Reachability *reachability;
 @end
 
 @implementation M80ServerViewController
 
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     
     self.navigationItem.title = NSLocalizedString(@"文件传输", nil);
     self.view.backgroundColor = M80RGB(0xEDEDED);
 
     _server = [[M80HttpServer alloc] init];
-    
-    
-    
-    
-    _linkLabel.text = [_server url];
     
     [_filesButton setBackgroundImage:[[UIColor orangeColor] m80ToImage]
                             forState:UIControlStateNormal];
@@ -41,18 +44,35 @@
     [_filesButton.layer setCornerRadius:buttonSize.width / 2];
     [_filesButton.layer setMasksToBounds:YES];
     [_filesButton.layer setBorderColor:[UIColor whiteColor].CGColor];
+
+    [self resetNetworkUI];
     
+    _reachability = [Reachability reachabilityForInternetConnection];
+    [_reachability startNotifier];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(onNetChanged:)
+                                                 name:kReachabilityChangedNotification
+                                               object:nil];
+}
+
+- (void)resetNetworkUI
+{
+    _linkLabel.text = [_server url];
     NSString *ssid = [[M80NetworkConfig currentConfig] currentSSID];
-    
     _networkLabel.text = !ssid ? NSLocalizedString(@"未使用Wifi", nil) :
     [NSString stringWithFormat:@"%@:%@", NSLocalizedString(@"当前网络", nil),ssid];
-    
-    
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 
+}
+
+- (void)onNetChanged:(NSNotification *)aNotification
+{
+    [self resetNetworkUI];
 }
 
 - (IBAction)showFiles:(id)sender {
--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
 pod 'GCDWebServer/WebUploader', '~> 3.2.1'
 pod 'M80Kit', '~> 1.0.4'
 pod 'Toast', '~> 2.4'
+pod 'Reachability', '~> 3.2'
